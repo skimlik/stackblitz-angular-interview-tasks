416129e0944b89dcc05e9880f836d70e5e2430a2
Serhii Kimlyk <serhii.kimlyk@wolterskluwer.com>
2025-08-19 21:44:49 +0300

    feat: adding task1 example

--- src/task1/task1.component.ts
+++ src/task1/task1.component.ts
@@ -1,21 +1,46 @@
 import { CommonModule } from "@angular/common";
-import { ChangeDetectionStrategy, Component, OnChanges, SimpleChanges } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnChanges,
+  SimpleChanges,
+} from "@angular/core";
+import { fromEvent, Subject, takeUntil } from "rxjs";
 
 @Component({
-    selector: 'app-task1-component',
-    standalone: true,
-    imports: [CommonModule],
-    templateUrl: 'task1.component.html',
-    styleUrl: 'task1.component.scss',
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: "app-task1-component",
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: "task1.component.html",
+  styleUrl: "task1.component.scss",
+  changeDetection: ChangeDetectionStrategy.Default,
 })
 export class Task1Component implements OnChanges {
-    ngOnChanges(changes: SimpleChanges): void {
-        console.log(`On Changes: ${Object.keys(changes).join(',')}`);
-    }
+  private _destroyed$ = new Subject<void>();
+  counter = 0;
 
-    ngOnInit(): void {
-        // Will it be ever triggered? component does not implement OnInit
-        console.log('On Init');
-    }
+  incrementOnClick$ = fromEvent(document, "click");
+
+  ngOnChanges(changes: SimpleChanges): void {
+    // Will it be ever triggered? In this particular case, no input properties are defined
+    console.log(`On Changes: ${Object.keys(changes).join(",")}`);
+  }
+
+  ngOnInit(): void {
+    // Will it be ever triggered? component does not implement OnInit
+    console.log("On Init");
+
+    this.incrementOnClick$.pipe(takeUntil(this._destroyed$)).subscribe(() => {
+      // Is this value captured in the browser {{ counter }} binding? And why?
+      this.counter++;
+      console.log(`Counter: ${this.counter}`);
+    });
+  }
+
+  ngOnDestroy(): void {
+    // Will it ever be triggered? component does not implement OnDestroy
+    this._destroyed$.next();
+    this._destroyed$.complete();
+    console.log("On Destroy");
+  }
 }
